3f4ca69ca20812686160483103cda6008aecabc6
Amzzy78 <murphy.annmarie78@gmail.com>
2021-09-27 14:24:00 +0000

    Create def get_purchases_data function to get data string from user

--- run.py
+++ run.py
@@ -12,8 +12,26 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS) 
 SHEET = GSPREAD_CLIENT.open('MunsterTv-Suppliers')  
 
-purchases = SHEET.worksheet('purchases')
+#purchases = SHEET.worksheet('purchases')
+#data = purchases.get_all_values()
+#print(data)
 
-data = purchases.get_all_values()
 
-print(data)
+
+# Requesting data from the user
+# Function to collect purchase data from user
+def get_purchases_data():
+    """
+    Get purchase figures input form the user
+    """
+    print("Please enter purchases data from previous month.")
+    print("Data covers 6 cells.")
+    print("Example: Date,Product,Quantity,Net Price,Tax and Supplier.\n")
+
+    data_str = ("Enter your data here: ")
+    print(f"The data provided is {data_str}")
+
+
+ # Call def get_purchases_data function  
+get_purchases_data() 
+
