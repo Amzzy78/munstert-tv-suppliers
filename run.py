4c418455c89e6d9af4a4263a534b64693bc22cf5
Amzzy78 <murphy.annmarie78@gmail.com>
2021-09-27 15:33:53 +0000

    Add user request loop to repeat for data until data provided is valid

--- run.py
+++ run.py
@@ -24,38 +24,50 @@ def get_purchases_data():
     """
     Get purchase figures input form the user
     """
-    print("Please enter purchases data from previous month.")
-    print("Data covers 6 cells and to be seperated by commas.")
-    print("Example: Date,Product,Quantity,Net Price,Tax and Supplier.\n")
+# Repeat request for data to loop until correct input
+    while True:
+        print("Please enter purchases data from previous month.")
+        print("Data covers 6 cells and to be seperated by commas.")
+        print("Example: Date,Product,Quantity,Net Price,Tax and Supplier.\n")
 
-    data_str = input("Enter your data here: ")
-    #print(f"The data provided is {data_str}")
+        data_str = input("Enter your data here: ")
+        #print(f"The data provided is {data_str}")
 
-    # Split() method returns the broken up values as a list
-    purchases_data = data_str.split(",")
-    #print(purchases_data)
+        # Split() method returns the broken up values as a list
+        purchases_data = data_str.split(",")
+        #print(purchases_data)
 
     # Call validate function
-    validate_data(purchases_data)
-    
+    #validate_data(purchases_data)
+
+        if validate_data(purchases_data):
+             print("Data is valid!")
+             break
+
+    return purchases_data
+
 # Validate data function with a try statement
 def validate_data(values):
     """
-    Inside the try, converts all string values into integers.
-    Raiss ValueError if strings cannot be converted into int,
+    Inside the try, converts all string values().
+    Raise ValueError if strings cannot be converted into int,
     or if there arent exactly 6 cell values.
     """
     try:
+        #[int(value) for value in values]  #only use if all numbers
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 cells with values required, you provided {len(values)}"
             )
     except ValueError as e:
-        print(f"Invalid data: {e}, please try again.\n")       
+        print(f"Invalid data: {e}, please try again.\n")  
+        return False   
 
-    print(values)    
+    return True         
+
+    #print(values)    
 
 
  # Call def get_purchases_data function  
-get_purchases_data() 
+data = get_purchases_data() 
 
